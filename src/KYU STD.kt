50cce71fa84cb8ed410e709d46717c1b0b9d0734
MAC <mac@MACs-MacBook-Air.local>
2022-04-13 12:10:45 +0300

    Initial commit

---
+++ src/KYU STD.kt
@@ -0,0 +1,15 @@
+fun main(){
+    val cups =20
+    val plates =60
+    if (cups<plates)
+    {
+        print("Too Many plates Less Cups")
+    }
+    else if(cups == plates){
+        print("Same Number of Cups and Plates")
+    }
+    else
+    {
+        print("Too Many Cups Less Plates")
+    }
+}
